076a7f701555a60478951c54bf44bd91f128bd47
Mattew Kline <mekline.jobs@gmail.com>
2017-07-07 16:12:36 -0400

    Trying to make a singleton

---
+++ ConsoleApp.Shapes/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Shapes
+{
+    public class Circle : Shape
+    {
+        private static Circle cir = null;
+        //private Circle(int height, int width) : base(int length, int width)
+    }
+}
---
+++ ConsoleApp.Shapes/SingletonShapeList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Shapes
+{
+    public class SingletonShapeList
+    {
+        private static List<Shape> shapeList; //NOT TECHNICALLY SINGLETON, Not SingletonShapeList type
+
+        private SingletonShapeList()
+        {
+            shapeList = new List<Shape>();
+        }
+        public List<Shape> list
+        {
+            get
+            {
+                if (shapeList == null)
+                    shapeList = new List<Shape>();
+                return shapeList;
+            }
+        }
+    }
+}
